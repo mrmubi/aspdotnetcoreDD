f42be15d5c4449536ccbcd40c4ec2fc4f7eb5523
Mubi <mrmubi@gmail.com>
2019-02-20 20:39:53 +0000

    Add files via upload

---
+++ Application/aspnet-core-dotnet-core/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace aspnet_core_dotnet_core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights("7a58d644-a8a1-4aae-b2d3-343c2cec1745")
+                .Build();
+
+            
+
+            host.Run();
+        }
+    }
+}
